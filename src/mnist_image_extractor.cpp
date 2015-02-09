7c1817b1948109b454f8fbf84e826fc9a6488fe0
Vadym Ostanin <v.ostanin@samsung.com>
2015-02-09 09:18:20 +0200

    correct crash at alloca
    Signed-off-by: Vadym Ostanin <v.ostanin@samsung.com>

--- src/mnist_image_extractor.cpp
+++ src/mnist_image_extractor.cpp
@@ -9,8 +9,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <png++/png.hpp>
 #include <png++/types.hpp>
+#include <sys/stat.h>
+#include <sys/types.h>
 using namespace std;
 
 int main()
@@ -27,6 +30,13 @@ int main()
 	int columns_count = 0;
 
 	int labels_count = 0;
+
+	for( int folder_i = 0 ; folder_i < 10 ; folder_i++ )
+	{
+		char label_name[PATH_MAX] = { '\0' };
+		sprintf( label_name, "./%d", folder_i );
+		mkdir( label_name, 0777 );
+	}
 
 	{
 		train_images_file_stream.read( ((char*)&magic) + 3, 1 );
@@ -61,14 +71,21 @@ int main()
 		train_labels_file_stream.read( ((char*)&labels_count) + 0, 1 );
 	}
 
+	stringstream file_name_str_stream;
+
 	for( int image_i = 0 ; image_i < images_count ; image_i++ )
 	{
 		char label = '\0';
 		train_labels_file_stream.read( &label, 1 );
 
-		char file_name[PATH_MAX] = { '\0' };
-		sprintf( file_name, "./image_%d_%d.png", label, image_i );
-		ofstream png_file_stream( file_name );
+		file_name_str_stream.clear();
+		file_name_str_stream.str( std::string() );
+
+		file_name_str_stream << "./" << (char)( 48 + label ) << "/image_" << image_i << ".png" << flush;
+
+		string file_name = file_name_str_stream.str();
+
+		ofstream png_file_stream( file_name.c_str() );
 		if( false == png_file_stream.is_open() )
 		{
 			return 0;
@@ -81,9 +98,9 @@ int main()
 		writer.set_bit_depth( 8 );
 		writer.write_info();
 
+		unsigned char * row = (unsigned char*)alloca( columns_count );
 		for( int row_i = 0 ; row_i < rows_count ; row_i++ )
 		{
-			unsigned char * row = (unsigned char*)alloca( rows_count );
 			for( int column_i = 0 ; column_i < columns_count ; column_i++ )
 			{
 				char pixel = '\0';
